af11e51e1803e7319eeb63328a87376cd8680e27
Ivonne <ivonnebenites2015@gmail.com>
2024-01-10 01:25:41 -0500

    Update file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  devise_scope :user do
+    authenticated :user do
+      root to: 'foods#index', as: :authenticated_root
+    end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+    unauthenticated :user do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
+
+  resources :recipes do
+    resources :recipe_foods
+  end
+
+  resources :inventories
+
+  get 'shopping_list', to: 'recipes#shopping_list', as: 'shopping_list'
+  
+  resources :foods
 end
